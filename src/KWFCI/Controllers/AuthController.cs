7d3d3f2e28efca2f7371593e5cb9a4da6d2d0449
Aaron Flager <aaron.flager@gmail.com>
2017-04-11 13:51:37 -0700

    controller/view/repo updated

---
+++ src/KWFCI/Controllers/AuthController.cs
@@ -0,0 +1,52 @@
+﻿using KWFCI.Models;
+using KWFCI.Models.ViewModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace KWFCI.Controllers
+{
+    public class AuthController : Controller
+    {
+        private UserManager<StaffUser> userManager;
+        private SignInManager<StaffUser> signInManager;
+
+        public AuthController(UserManager<StaffUser> usrMgr, SignInManager<StaffUser> sim)
+        {
+            userManager = usrMgr;
+            signInManager = sim;
+        }
+
+        [AllowAnonymous]
+        public ViewResult Login(string returnUrl)
+        {
+            return View(new LoginVM());
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        public async Task<IActionResult> Login(LoginVM vm)
+        {
+            if (ModelState.IsValid)
+            {
+                StaffUser user = await userManager.FindByEmailAsync(vm.Email);
+                if (user != null)
+                {
+                    await signInManager.SignOutAsync();
+                    Microsoft.AspNetCore.Identity.SignInResult result =
+                            await signInManager.PasswordSignInAsync(
+                                user, vm.Password, false, false);
+                    if (result.Succeeded)
+                    {
+                        //Redirects to the home index if login succeeds
+                        return Redirect("/");
+                    }
+                }
+                ModelState.AddModelError(nameof(LoginVM.Email),
+                    "Invalid email or password");
+            }
+            return View(vm);
+        }
+    }
+}
---
+++ src/KWFCI/Repositories/StaffProfileRepository.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using KWFCI.Models;
+
+namespace KWFCI.Repositories
+{
+    public class StaffProfileRepository : IStaffProfileRepository
+    {
+        private ApplicationDbContext context;
+
+        public StaffProfileRepository(ApplicationDbContext ctx)
+        {
+            context = ctx;
+        }
+
+        public IQueryable<StaffProfile> GetAllStaffProfiles()
+        {
+            throw new NotImplementedException();
+        }
+
+        public StaffProfile GetStaffProfileByFullName(string firstName, string lastName)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
