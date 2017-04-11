3df9452807d5e80dfd0c97b284f6b7e6a83783d8
Aaron Flager <aaron.flager@gmail.com>
2017-04-11 14:15:12 -0700

    added profile info to seed data

--- src/KWFCI/Repositories/ApplicationDbContext.cs
+++ src/KWFCI/Repositories/ApplicationDbContext.cs
@@ -1,9 +1,11 @@
 ﻿using KWFCI.Models;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 
 namespace KWFCI.Repositories
 {
     public class ApplicationDbContext : IdentityDbContext<StaffUser>
     {
+        public DbSet<StaffProfile> StaffProfiles { get; set; }
     }
 }
