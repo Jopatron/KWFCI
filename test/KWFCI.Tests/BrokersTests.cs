eb7e586f96bac634abbfc36c5239ecc8e1af4b73
JGarnick <garnick.josh@gmail.com>
2017-04-13 16:28:31 -0700

    Created fake brokers repository

---
+++ test/KWFCI.Tests/BrokersTests.cs
@@ -0,0 +1,15 @@
+﻿using KWFCI.Controllers;
+using KWFCI.Tests.FakeRepositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KWFCI.Tests
+{
+    public class BrokersTests
+    {
+        FakeBrokerRepository repo;
+        BrokersController controller;
+    }
+}
---
+++ test/KWFCI.Tests/FakeRepositories/FakeBrokerRepository.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using KWFCI.Models;
+using KWFCI.Repositories;
+
+namespace KWFCI.Tests.FakeRepositories
+{
+    public class FakeBrokerRepository : IBrokerRepository
+    {
+        public List<Broker> brokers = new List<Broker>();
+
+        public FakeBrokerRepository()
+        {
+            Broker broker = new Broker { FirstName = "Lonny", LastName = "Jenkins", Email = "ljenkins@kw.com", EmailNotifications = true, Type = "New Broker" };
+            brokers.Add(broker);
+
+            broker = new Broker { FirstName = "Samantha", LastName = "Coldwater", Email = "scoldwater@kw.com", EmailNotifications = true, Type = "In Transition" };
+            brokers.Add(broker);
+
+            broker = new Broker { FirstName = "Brooke", LastName = "Schelling", Email = "bschelling@kw.com", EmailNotifications = true, Type = "New Broker" };
+            brokers.Add(broker);
+        }
+
+
+
+        public int AddBroker(Broker broker)
+        {
+            brokers.Add(broker);
+            if (brokers.Contains(broker))
+                return 1;
+            else
+                return 0;
+        }
+
+        public int DeleteBroker(Broker broker)
+        {
+            brokers.Remove(broker);
+            if (!brokers.Contains(broker))
+                return 1;
+            else
+                return 0;
+        }
+
+        public IQueryable<Broker> GetAllBrokers()
+        {
+            return brokers.AsQueryable();
+        }
+
+        public Broker GetBrokerByID(int id)
+        {
+            return brokers.Where(b => b.BrokerID == id) as Broker;
+        }
+
+        public IQueryable<Broker> GetBrokersByType(string type)
+        {
+            return brokers.Where(b => b.Type == type).AsQueryable();
+        }
+
+        public int UpdateBroker(Broker newBroker)
+        {
+            Broker oldBroker = brokers.Where(b => b.BrokerID == newBroker.BrokerID) as Broker;
+            oldBroker.Type = newBroker.Type;
+            oldBroker.FirstName = newBroker.FirstName;
+            oldBroker.LastName = newBroker.LastName;
+            oldBroker.Email = newBroker.Email;
+            oldBroker.EmailNotifications = newBroker.EmailNotifications;
+
+            int index = brokers.IndexOf(oldBroker);
+            brokers.Remove(oldBroker);
+            brokers.Insert(index, oldBroker);
+
+            if (brokers.IndexOf(oldBroker) == index)
+                return 1;
+            else
+                return 0;
+        }
+    }
+}
