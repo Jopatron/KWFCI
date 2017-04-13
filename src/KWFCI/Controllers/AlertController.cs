0c002c9c1ed946dfd2d7fc9030291aad2ba6ef20
Aaron Flager <aaron.flager@gmail.com>
2017-04-12 18:46:27 -0700

    Alert view/model/controller working

---
+++ src/KWFCI/Controllers/AlertController.cs
@@ -0,0 +1,45 @@
+﻿using KWFCI.Models;
+using KWFCI.Repositories;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KWFCI.Controllers
+{
+    [Authorize(Roles = "Staff")]
+    [Route("Alerts")]
+    public class AlertController : Controller
+    {
+        private IAlertRepository alertRepo;
+
+        public AlertController(IAlertRepository repo)
+        {
+            alertRepo = repo;
+        }
+
+        public IActionResult AllAlerts()
+        {
+            var allAlerts = alertRepo.GetAllAlerts().ToList();
+            return View(allAlerts);
+        }
+
+        [Route("Add")]
+        [HttpPost]
+        public IActionResult AddAlert(Alert a)
+        {
+            var alert = new Alert
+            {
+                Priority = a.Priority,
+                AlertDate = a.AlertDate,
+                Message = a.Message
+            };
+
+            alertRepo.AddAlert(alert);
+            //TODO: See if there is a way to just close the modal and not refresh the page
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
--- src/KWFCI/Models/Alert.cs
+++ src/KWFCI/Models/Alert.cs
@@ -1,11 +1,12 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace KWFCI.Models
 {
     public class Alert
     {
+        public int AlertID { get; set; }
+        public DateTime AlertDate { get; set; }
+        public int Priority { get; set; }
+        public string Message { get; set; }
     }
 }
