f9a4272ca1e589d27da417fe2f260f073edf7d77
Patrick Bailey <patrickbailey13@gmail.com>
2017-04-17 11:01:46 -0700

    sets up controller method for displaying all messages

---
+++ src/KWFCI/Controllers/MessagesController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using KWFCI.Repositories;
+
+namespace KWFCI.Controllers
+{
+    [Authorize(Roles = "Staff")]
+    [Route("Messages")]
+    public class MessagesController : Controller
+    {
+        private IMessageRepository messageRepo;
+
+        public MessagesController(IMessageRepository repo)
+        {
+            messageRepo = repo;
+        }
+
+        
+        public ViewResult AllMessages()
+        {
+            var allMessages = messageRepo.GetAllMessages().ToList();
+            return View(allMessages);
+        }
+    }
+}
