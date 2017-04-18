d444c9f9027db21463eb77bcefc214a04ee90e18
JGarnick <garnick.josh@gmail.com>
2017-04-18 16:30:08 -0700

    Added Migration for Interactions

---
+++ src/KWFCI/Migrations/20170418232128_AddedStatusToInteraction.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace KWFCI.Migrations
+{
+    public partial class AddedStatusToInteraction : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Status",
+                table: "Interactions",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Status",
+                table: "Interactions");
+        }
+    }
+}
