f168b8123bbfbba7a77616ea33b4ae961a71b879
Aaron Flager <aaron.flager@gmail.com>
2017-04-18 14:56:06 -0700

    Controller and view added

---
+++ src/KWFCI/Controllers/InteractionsController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+
+namespace KWFCI.Controllers
+{
+    public class InteractionsController : Controller
+    {
+        public IActionResult Index()
+        {
+            //TODO Ensure user is rerouted if not logged in
+            return View("AllInteractions");
+        }
+    }
+}
--- src/KWFCI/Startup.cs
+++ src/KWFCI/Startup.cs
@@ -44,8 +44,7 @@ namespace KWFCI
             services.AddTransient<IBrokerRepository, BrokerRepository>();
             services.AddTransient<IStaffProfileRepository, StaffProfileRepository>();
             services.AddTransient<IAlertRepository, AlertRepository>();
-            services.AddTransient<IMessageRepository, MessageRepository>();
-            
+            services.AddTransient<IMessageRepository, MessageRepository>();            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
