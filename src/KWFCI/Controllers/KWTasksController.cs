34e7f1d0ca10cbae9b82815f3916f16f6630774d
Aaron Flager <aaron.flager@gmail.com>
2017-04-19 23:25:48 -0700

    Controller adapted, views not yet modified

--- src/KWFCI/Controllers/KWTasksController.cs
+++ src/KWFCI/Controllers/KWTasksController.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using KWFCI.Models;
+using KWFCI.Models.ViewModels;
+using KWFCI.Repositories;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -6,8 +10,98 @@ using System.Threading.Tasks;
 
 namespace KWFCI.Controllers
 {
+    [Authorize(Roles = "Staff")]
+    [Route("KWTasks")]
     public class KWTasksController : Controller
     {
+        private IKWTaskRepository kwtRepo;
 
-    }
+        public KWTasksController(IKWTaskRepository repo)
+        {
+            kwtRepo = repo;
+        }
+
+        public IActionResult AllKWTasks()
+        {
+            var allKWTasks = kwtRepo.GetAllKWTasks().ToList();
+            var vm = new KWTaskVM();
+            vm.KWTasks = allKWTasks;
+            return View(vm);
+        }
+
+        [Route("Add")]
+        [HttpPost]
+        public IActionResult AddKWTask(KWTaskVM vm)
+        {
+            var kwt = vm.NewKWTask;
+            var kwtask = new KWTask
+            {
+                Description = kwt.Description,
+                StaffProfile = kwt.StaffProfile,
+                Alert = kwt.Alert
+            };
+
+            kwtRepo.AddKWTask(kwtask);
+            //TODO: See if there is a way to just close the modal and not refresh the page
+            return RedirectToAction("AllKWTasks");
+        }
+
+        [HttpPost]
+        public IActionResult Delete(int id)
+        {
+            KWTask kwtask = kwtRepo.GetKWTaskByID(id);
+            if (kwtask != null)
+            {
+                kwtRepo.DeleteKWTask(kwtask);
+                return RedirectToAction("AllKWTasks");
+            }
+            else
+            {
+                ModelState.AddModelError("", "Task Not Found");
+            }
+            return RedirectToAction("AllKWTasks");
+        }
+
+        [Route("Edit")]
+        public ActionResult Edit(int id)
+        {
+            KWTask kwtask = kwtRepo.GetKWTaskByID(id);
+            if (kwtask != null)
+            {
+                return View(kwtask);
+            }
+            else
+            {
+                return RedirectToAction("AllKWTasks");
+            }
+        }
+
+        [Route("Edit")]
+        [HttpPost]
+        public IActionResult Edit(KWTask kwt)
+        {
+            if (kwt != null)
+            {
+                KWTask kwtask = kwtRepo.GetKWTaskByID(kwt.KWTaskID);
+                kwtask.Description = kwt.Description;
+                kwtask.StaffProfile = kwt.StaffProfile;
+                kwtask.Alert = kwt.Alert;
+
+                int verify = kwtRepo.UpdateKWTask(kwtask);
+                if (verify == 1)
+                {
+                    //TODO add feedback of success
+                    return RedirectToAction("AllKWTasks");
+                }
+                else
+                {
+                    //TODO add feedback for error
+                }
+            }
+            else
+            {
+                ModelState.AddModelError("", "Task Not Found");
+            }
+            return View(kwt);
+        }
 }
