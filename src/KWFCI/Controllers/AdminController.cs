1d875b29fc045d380f2e8ede8ed02ce4806a7ee2
Aaron Flager <aaron.flager@gmail.com>
2017-04-26 16:53:43 -0700

    Admin Interaction viewmodel, view, and controller basics. Doesn't include staff to interaction information yet.

---
+++ src/KWFCI/Controllers/AdminController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using KWFCI.Repositories;
+using KWFCI.Models.ViewModels;
+
+namespace KWFCI.Controllers
+{
+    [Route("Admin")]
+    public class AdminController : Controller
+    {
+        private IInteractionsRepository intRepo;
+
+        public AdminController(IInteractionsRepository repo)
+        {
+            intRepo = repo;
+        }
+
+        [Route("Interactions")]
+        public IActionResult AdminInteractions()
+        {
+            var vm = new AdminInteractionVM();
+            vm.Interactions = intRepo.GetAllInteractions().ToList();
+            //TODO Ensure user is rerouted if not logged in
+            return View(vm);
+        }
+    }
+}
---
+++ src/KWFCI/Models/ViewModels/AdminInteractionVM.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KWFCI.Models.ViewModels
+{
+    public class AdminInteractionVM
+    {
+        public List<Interaction> Interactions { get; set; }
+    }
+}
