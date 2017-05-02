daf3c2b270443205d4adbe005bb14f864844324e
Patrick Bailey <patrickbailey13@gmail.com>
2017-05-02 15:25:37 -0700

    binds logged in profile to the view

--- src/KWFCI/Controllers/SettingsController.cs
+++ src/KWFCI/Controllers/SettingsController.cs
@@ -3,14 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
 
 namespace KWFCI.Controllers
 {
+    [Authorize(Roles = "Staff")]
     public class SettingsController : Controller
     {
+        
         public IActionResult Index()
         {
-            return View();
+            if (Helper.StaffProfileLoggedIn == null)
+                return RedirectToAction("Login", "Auth");
+            // TODO: Grab logged in user profile
+            var profile = Helper.StaffProfileLoggedIn;
+            // TODO: Send Staff Profile to View
+            return View(profile);
         }
     }
 }
