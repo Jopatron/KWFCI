80c64255362d7b4797d5b91ddf48b20406ebefe7
Patrick Bailey <patrickbailey13@gmail.com>
2017-05-09 16:35:18 -0700

    style changes for new broker modal and modal-content class

--- src/KWFCI/Controllers/InteractionsController.cs
+++ src/KWFCI/Controllers/InteractionsController.cs
@@ -150,8 +150,7 @@ namespace KWFCI.Controllers
                         
                 }
                     
-                else if (iVM.Field == "Date Created")
-                    interaction.DateCreated = i.DateCreated;
+                    
                 
                 
                 
