f8faa741ed22a4340b02e1d5698767d791eb0011
Patrick Bailey <patrickbailey13@gmail.com>
2017-05-09 22:03:20 -0700

    finished up logic for update button

--- src/KWFCI/Controllers/InteractionsController.cs
+++ src/KWFCI/Controllers/InteractionsController.cs
@@ -102,7 +102,25 @@ namespace KWFCI.Controllers
             return RedirectToAction("BrokerInteractions", new {BrokerID = BrokerID });
 
         }
+        [Route("Onboarding")]
+        public IActionResult UpdateTasks()
+        {
+            var vm = new InteractionVM();
+            return View(vm);
+        }
 
+        [Route("Onboarding")]
+        [HttpPost]
+        public IActionResult UpdateTasks(InteractionVM vm)
+        {
+            foreach (KWTask t in vm.Tasks)
+            {
+                var task = taskRepo.GetKWTaskByID(t.KWTaskID);
+                task.IsComplete = t.IsComplete;
+                taskRepo.UpdateKWTask(task);
+            }
+            return RedirectToAction("BrokerInteractions", new { BrokerID = vm.BrokerID });
+        }
         [Route("Edit")]
         [HttpPost]
         public ActionResult Edit(InteractionVM iVM, string taskAction = "")
