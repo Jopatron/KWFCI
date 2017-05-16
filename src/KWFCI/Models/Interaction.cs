e5029fb2f05ea9ccd05b6407078bc447f0774fb0
Patrick Bailey <patrickbailey13@gmail.com>
2017-05-16 13:51:46 -0700

    validation for next steps

--- src/KWFCI/Models/Interaction.cs
+++ src/KWFCI/Models/Interaction.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-
+using System.ComponentModel.DataAnnotations;
 
 namespace KWFCI.Models
 {
@@ -8,6 +8,8 @@ namespace KWFCI.Models
         public int InteractionID { get; set; }
         public string Notes { get; set; }
         public DateTime? DateCreated { get; set; }
+
+        [StringLength(1000, MinimumLength = 10,ErrorMessage ="Next Step field must be between 10 and 1,000 characters")]
         public string NextStep { get; set; }
         public int? TaskForeignKey { get; set; }
         public string Status { get; set; }
