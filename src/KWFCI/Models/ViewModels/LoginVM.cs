866e0c815f224262a6a19e43505391745f56a136
Patrick Bailey <patrickbailey13@gmail.com>
2017-05-16 14:11:57 -0700

    login validation

--- src/KWFCI/Models/ViewModels/LoginVM.cs
+++ src/KWFCI/Models/ViewModels/LoginVM.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,7 +8,9 @@ namespace KWFCI.Models.ViewModels
 {
     public class LoginVM
     {
+        [Required(ErrorMessage = "You must enter a username")]
         public string UserName { get; set; }
+        [Required(ErrorMessage = "You must enter a password")]
         public string Password { get; set; }
     }
 }
